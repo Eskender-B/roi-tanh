527f7a78bc9c1b86dd313fc7e90981c8008f9679
Eskender <eskenderbesrat@gmail.com>
2019-10-18 15:16:09 +0800

    test

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from scipy.ndimage import map_coordinates
 
 detector = MTCNN()
 
-img = io.imread('img.jpg')
+img = io.imread('img3.jpg')
 h,w,c = img.shape
 res = detector.detect_faces(img)[0]['keypoints']
 rows, cols = [], []
@@ -18,6 +18,8 @@ for v in res.values():
 	rows.append(v[0])
 	cols.append(v[1])
 
+print('left', res['left_eye'])
+print('right', res['right_eye'])
 src = np.array([res['left_eye'], res['right_eye'], res['nose'], res['mouth_left'], res['mouth_right']])
 dst = np.array([[-0.25,-0.1], [0.25, -0.1], [0.0, 0.1], [-0.15, 0.4], [0.15, 0.4]])
 tform = transform.estimate_transform('similarity', src, dst)
@@ -25,15 +27,15 @@ tform = transform.estimate_transform('similarity', src, dst)
 rec = [[-1.,1.], [1.,1.], [1.,-1.], [-1.,-1.]]
 rect = tform.inverse(rec)
 
-sampleX = np.expand_dims(np.arctanh(np.linspace(-.99, .99, 512)),-1).repeat(512,-1)
+sampleX = np.expand_dims(np.arctanh(np.linspace(-.99, .99, 512)), 0).repeat(512, 0)
 sampleY = np.expand_dims(np.arctanh(np.linspace(-.99, .99, 512)),-1).repeat(512,-1)
 
-coords = np.stack([sampleY, sampleX]).transpose(1,2,0)
-print(coords.reshape(-1,2)[512*512//2:512*512//2+10])
+coords = np.stack([sampleX, sampleY]).transpose(1,2,0)
 
 
-coords = tform.inverse(coords.reshape(-1,2)).reshape(512,512,2).transpose(2,0,1)
-print(coords[0,256:266,256:266])
+
+coords = np.flip(tform.inverse(coords.reshape(-1,2)).reshape(512,512,2).transpose(2,0,1), 0)
+
 
 warped0 = map_coordinates(img[:,:,0], coords)
 warped1 = map_coordinates(img[:,:,1], coords)
